cf94d653f14aa80b1c79ab0edbaf850e02a10863
Brian Zhou <brhzhou@gmail.com>
2022-04-03 17:47:05 -0700

    change

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
+#include <Arduino.h>
+#include "States/States.h"
+#include "Telemetry/Telemetry.h"
+#include "Vec3/Vec3.h"
+
+
+
 void setup() {
+  // initialize
+  States STATE;
+  Telemetry TELE;
+  // @ SD Card Init
 
-}
-
-void loop() {
+  switch (STATE)
+  {
+  case GROUND_IDLE:
+    break;
   
+  default:
+    break;
+  }
+
 }
+
+void loop() {}   
