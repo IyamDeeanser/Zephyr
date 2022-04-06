ac4b257348a15034c00dfe3329940eecd2d5c45e
IyamDeeanser <TimothyCai2004@gmail.com>
2022-04-06 12:39:53 -0700

    deleted all to revert change

--- Zephyr/README.md
+++
@@ -1,9 +0,0 @@
-# Zephyr
-Team Zephyr's flight and GCS code repository for the 2022 Canadian CanSat Design Challenge.
-
-**Team Members:**
-- Timothy Cai
-- Brian Zhou
-- Jessica Tang
-- Vhea He
- 
--- Zephyr/examples/Timer/Timer.cpp
+++
@@ -1,9 +0,0 @@
-#include "Timer.h"
-#include "Arduino.h"
-
-void Time::update(){
-    previousTime = currentTime;
-    currentTime = micros();
-    deltaTimeMicro = currentTime - previousTime;
-    deltaTimeSec = deltaTimeMicro / 1000000.0;
-}
