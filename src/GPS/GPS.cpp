0ca22fa6078a1873d9e738926abdd2804ea5c193
TangerineSoda <98002993+TangerineSoda@users.noreply.github.com>
2022-04-07 00:03:20 -0700

    Update GPS.cpp

---
+++ src/GPS/GPS.cpp
@@ -0,0 +1,50 @@
+//GPS Implementation File
+//Written by: Vhea He
+
+#include <GPS.h>
+
+GPS_Stats::GPS_Stats(){
+
+    //Initializes everything to 0
+        //If GPS not responding then all variables will be equal to zero, and it will be obvious there is a problem
+    latitude = 0.0;
+    longitude = 0.0;
+    altitude = 0.0;
+    speed = 0.0;
+    angle= 0.0;
+    numSatellites = 0;
+
+    ZephyrGPS.begin(0x10);
+
+    //turns on RMC (recommended minimum) and GGA (fix data) including altitude
+    ZephyrGPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  
+    ZephyrGPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+
+    // Request updates on antenna status 
+    ZephyrGPS.sendCommand(PGCMD_ANTENNA);
+
+    delay(1000);
+    
+    // Ask for firmware version
+    ZephyrGPS.println(PMTK_Q_RELEASE);
+}
+
+void GPS_Stats::update(){
+
+    //Reads NMEA character by character 
+    char c = ZephyrGPS.read();
+    // if a sentence is received, we can check the checksum, parse it...
+    if (ZephyrGPS.newNMEAreceived()) {
+        // we can fail to parse a sentence in which case we should just wait for another
+        if (!ZephyrGPS.parse(ZephyrGPS.lastNMEA())) {return;} 
+    }
+
+    //Updates each variable with new information
+    latitude = ZephyrGPS.latitude;
+    longitude = ZephyrGPS.longitude;
+    altitude = ZephyrGPS.altitude;
+    speed = ZephyrGPS.speed;
+    angle= ZephyrGPS.angle;
+    numSatellites = ZephyrGPS.satellites;
+}
