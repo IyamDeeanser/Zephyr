6fe12fbea1eeeb0b7b2f91437d54ee6c492d775e
IyamDeeanser <TimothyCai2004@gmail.com>
2022-04-14 21:49:52 -0700

    updated quaternions to work with accel offsets

---
+++ test/quaternion_test_main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include "Quats/quaternions.h"
+#include "Timer/Timer.h"
+#include "IMU/IMU.h"
+
+
+Quat quaternion;
+IMU gyro;
+Timer timer1;
+
+void setup()
+{
+    gyro.begin();
+    gyro.getGyroBias();
+    quaternion.begin();
+    SerialUSB.begin(9600);
+}
+
+void loop()
+{
+    gyro.update();
+    timer1.update();
+    quaternion.calculateQuaternion(gyro, timer1);
+    SerialUSB.print(quaternion.pitch);      SerialUSB.print(", ");
+    SerialUSB.print(quaternion.yaw);        SerialUSB.print(", ");
+    SerialUSB.println(quaternion.roll); 
+}
