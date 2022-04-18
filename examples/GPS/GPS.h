35c28d081f017139f32efad448a2c8c0ef11d869
TangerineSoda <98002993+TangerineSoda@users.noreply.github.com>
2022-04-18 12:08:57 -0700

    Update GPS.h

--- examples/GPS/GPS.h
+++ examples/GPS/GPS.h
@@ -1,1 +1,32 @@
+#ifndef GPS_H
+#define GPS_H
 
+#include <Arduino.h>
+#include <Adafruit_GPS.h>
+#include <SPI.h>
+#include <Wire.h>
+
+// Set ZephyrGPSECHO to 'false' to turn off echoing the ZephyrGPS data to the Serial console
+#define ZephyrGPSECHO false
+
+class GPS_Stats{
+
+public:
+    //The GPS object
+    Adafruit_GPS ZephyrGPS;
+
+    //Value variables, public for simplicity 
+    float latitude;
+    float longitude;
+    float altitude;
+    float speed;
+    float angle;
+    int numSatellites;
+
+    //Constructor and update function declaration
+    GPS_Stats();
+    void begin();
+    void update();
+};
+
+#endif
