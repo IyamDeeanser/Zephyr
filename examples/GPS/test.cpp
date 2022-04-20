6044743d534ca0711b2e5b210e986c8c02379c4c
TangerineSoda <98002993+TangerineSoda@users.noreply.github.com>
2022-04-20 16:38:44 -0700

    Update test.cpp

--- examples/GPS/test.cpp
+++ examples/GPS/test.cpp
@@ -1,29 +1,32 @@
-#include "GPS.h"
+#include "GPS.cpp"
+#include "GPS Variables.cpp"
+#include <Adafruit_GPS.h>
 
-GPS_Stats ZephyrGPS;
+Adafruit_GPS ZephyrGPS;
+GPSVariables GPSVar;
 
 void setup(){
-    ZephyrGPS.begin();
+    GPSBegin(ZephyrGPS);
 }
 
 void loop(){
-    ZephyrGPS.update();
-    SerialUSB.print("-----------------\n RotaSat GPS Testing: \n -----------------\n");
-    SerialUSB.print("Location: \n -----------------\n");
-    SerialUSB.print("Latitude: ");
-    SerialUSB.print(ZephyrGPS.generalLat);
-    SerialUSB.print('\n');
-    SerialUSB.print("Longitude: ");
-    SerialUSB.print(ZephyrGPS.generalLon);
-    SerialUSB.print('\n');
-    SerialUSB.print("-----------------\nAltitude: ");
-    SerialUSB.print(ZephyrGPS.altitude); SerialUSB.print(" m");
-    SerialUSB.print("\nSpeed: ");
-    SerialUSB.print(ZephyrGPS.speed); SerialUSB.print(" knots");
-    SerialUSB.print("\nAngle: ");
-    SerialUSB.print(ZephyrGPS.angle); SerialUSB.print(" degrees");
-    SerialUSB.print("\nNumber of Satellites Connected: ");
-    SerialUSB.print(ZephyrGPS.numSatellites);
-    SerialUSB.print('\n');
-    delay(2000);
+    GPSUpdate(ZephyrGPS, GPSVar);
+    // SerialUSB.print("-----------------\n RotaSat GPS Testing: \n -----------------\n");
+    // SerialUSB.print("Location: \n -----------------\n");
+    // SerialUSB.print("Latitude: ");
+    // SerialUSB.print(ZephyrGPS.generalLat);
+    // SerialUSB.print('\n');
+    // SerialUSB.print("Longitude: ");
+    // SerialUSB.print(ZephyrGPS.generalLon);
+    // SerialUSB.print('\n');
+    // SerialUSB.print("-----------------\nAltitude: ");
+    // SerialUSB.print(ZephyrGPS.altitude); SerialUSB.print(" m");
+    // SerialUSB.print("\nSpeed: ");
+    // SerialUSB.print(ZephyrGPS.speed); SerialUSB.print(" knots");
+    // SerialUSB.print("\nAngle: ");
+    // SerialUSB.print(ZephyrGPS.angle); SerialUSB.print(" degrees");
+    // SerialUSB.print("\nNumber of Satellites Connected: ");
+    // SerialUSB.print(ZephyrGPS.numSatellites);
+    // SerialUSB.print('\n');
+    // delay(2000);
 }
