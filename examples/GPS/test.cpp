33d25477e008240d8e518334a658105b3c738de3
TangerineSoda <98002993+TangerineSoda@users.noreply.github.com>
2022-04-20 12:06:38 -0700

    Update test.cpp

--- examples/GPS/test.cpp
+++ examples/GPS/test.cpp
@@ -8,17 +8,22 @@ void setup(){
 
 void loop(){
     ZephyrGPS.update();
-
-    SerialUSB.print(ZephyrGPS.latitude);
+    SerialUSB.print("-----------------\n RotaSat GPS Testing: \n -----------------\n");
+    SerialUSB.print("Location: \n -----------------\n");
+    SerialUSB.print("Latitude: ");
+    SerialUSB.print(ZephyrGPS.generalLat);
     SerialUSB.print('\n');
-    SerialUSB.print(ZephyrGPS.longitude);
+    SerialUSB.print("Longitude: ");
+    SerialUSB.print(ZephyrGPS.generalLon);
     SerialUSB.print('\n');
-    SerialUSB.print(ZephyrGPS.altitude);
-    SerialUSB.print('\n');
-    SerialUSB.print(ZephyrGPS.speed);
-    SerialUSB.print('\n');
-    SerialUSB.print(ZephyrGPS.angle);
-    SerialUSB.print('\n');
+    SerialUSB.print("-----------------\nAltitude: ");
+    SerialUSB.print(ZephyrGPS.altitude); SerialUSB.print(" m");
+    SerialUSB.print("\nSpeed: ");
+    SerialUSB.print(ZephyrGPS.speed); SerialUSB.print(" knots");
+    SerialUSB.print("\nAngle: ");
+    SerialUSB.print(ZephyrGPS.angle); SerialUSB.print(" degrees");
+    SerialUSB.print("\nNumber of Satellites Connected: ");
     SerialUSB.print(ZephyrGPS.numSatellites);
-    delay(100);
+    SerialUSB.print('\n');
+    delay(2000);
 }
