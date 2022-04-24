8b5a96fb9d8b30637bb48f36c0e8363d960e5ac8
kentarouh <kentarou.howard65@outlook.com>
2022-04-24 16:19:23 -0700

    Upload telemetry test file

--- test/TLM_test/TLM_test.ino
+++ test/TLM_test/TLM_test.ino
@@ -1,4 +1,4 @@
-#define Serial SerialUSB
+//#define Serial1 Serial1USB
 int state = -1;
 float timeNow;
 float oldTime; 
@@ -7,7 +7,7 @@ float timeSec;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial1.begin(115200);
 }
 
 void loop() {
@@ -15,31 +15,31 @@ void loop() {
   if(timeNow - oldTime >= 66.67){
     oldTime = timeNow;
     timeSec = timeNow / 1000;
-    Serial.print("ROTATLM"); // prefix
-    Serial.print(random(-1000, 1000) * 0.01, 2);                 Serial.print(",");
-    Serial.print(random(-1000, 1000) * 0.01, 2);                 Serial.print(",");
-    Serial.print(random(-1000, 1000) * 0.01, 2);                 Serial.print(",");
-    Serial.print(random(970, 990) * 0.01, 2);               Serial.print(",");
-    Serial.print(random(-1000, 1000) * 0.001, 2);               Serial.print(",");
-    Serial.print(random(-1000, 1000) * 0.001, 2);               Serial.print(",");
-    Serial.print(random(-1000, 1000) * 0.001, 2);                Serial.print(",");
-    Serial.print(random(-1000, 1000) * 0.001, 2);                Serial.print(",");
-    Serial.print(random(-1000, 1000) * 0.001, 2);                Serial.print(",");
-    Serial.print(random(-1000, 1000) * 0.001, 2);              Serial.print(",");
-    Serial.print(random(-1000, 1000) * 0.001, 2);             Serial.print(",");
-    Serial.print(random(-2550, 2550) * 0.1, 2);               Serial.print(",");
-    Serial.print(random(8300, 8600) * 0.001, 2);        Serial.print(",");
-    Serial.print(state);              Serial.print(",");
-    Serial.print(1);              Serial.print(",");
-    Serial.print(1);       Serial.print(",");
-    Serial.print(timeSec, 3);             Serial.print(",");
-    Serial.print(timeSec, 3);         Serial.print(",");
-    Serial.print(random(101200, 101400) * 0.01, 3);              Serial.print(",");
-    Serial.print(random(240, 260) * 0.01, 2);               Serial.print(",");
-    Serial.print(random(340, 360) * 0.01, 2);              Serial.print(",");
-    Serial.print(random(13, 14));                  Serial.print(",");
-    Serial.print(random(496956,496996)*0.0001 , 4);              Serial.print(",");
-    Serial.println(random(1128451,1128751)*0.0001, 4);
+    Serial1.print("ROTATLM"); // prefix
+    Serial1.print(random(-1000, 1000) * 0.01, 2);                 Serial1.print(",");
+    Serial1.print(random(-1000, 1000) * 0.01, 2);                 Serial1.print(",");
+    Serial1.print(random(-1000, 1000) * 0.01, 2);                 Serial1.print(",");
+    Serial1.print(random(970, 990) * 0.01, 2);               Serial1.print(",");
+    Serial1.print(random(-1000, 1000) * 0.001, 2);               Serial1.print(",");
+    Serial1.print(random(-1000, 1000) * 0.001, 2);               Serial1.print(",");
+    Serial1.print(random(-1000, 1000) * 0.001, 2);                Serial1.print(",");
+    Serial1.print(random(-1000, 1000) * 0.001, 2);                Serial1.print(",");
+    Serial1.print(random(-1000, 1000) * 0.001, 2);                Serial1.print(",");
+    Serial1.print(random(-1000, 1000) * 0.001, 2);              Serial1.print(",");
+    Serial1.print(random(-1000, 1000) * 0.001, 2);             Serial1.print(",");
+    Serial1.print(random(-2550, 2550) * 0.1, 2);               Serial1.print(",");
+    Serial1.print(random(8300, 8600) * 0.001, 2);        Serial1.print(",");
+    Serial1.print(state);              Serial1.print(",");
+    Serial1.print(1);              Serial1.print(",");
+    Serial1.print(1);       Serial1.print(",");
+    Serial1.print(timeSec, 3);             Serial1.print(",");
+    Serial1.print(timeSec, 3);         Serial1.print(",");
+    Serial1.print(random(101200, 101400) * 0.01, 3);              Serial1.print(",");
+    Serial1.print(random(240, 260) * 0.01, 2);               Serial1.print(",");
+    Serial1.print(random(340, 360) * 0.01, 2);              Serial1.print(",");
+    Serial1.print(random(13, 14));                  Serial1.print(",");
+    Serial1.print(random(496956,496996)*0.0001 , 4);              Serial1.print(",");
+    Serial1.println(random(1128451,1128751)*0.0001, 4);
   }
 
   if(timeNow - stateTime >= 1000){
