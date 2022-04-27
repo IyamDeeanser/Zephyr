407fefeeb03873f01f336dc6026a76d9175c3b5f
kentarouh <kentarou.howard65@outlook.com>
2022-04-26 23:08:46 -0700

    Remove serial1 from SD

--- src/SD_Card/SD_File/SD_File.cpp
+++ src/SD_Card/SD_File/SD_File.cpp
@@ -55,32 +55,35 @@ void SD_File::eject() {
 }
 
 void SD_File::logData(vec3 ori, vec3 accel, vec3 accelHG, vec3 gyro, float altitude, float rwValue, float batteryVoltage, const int & systemState, const bool & cameraState, bool reactionWheelState, float onTimeSec, float flightTimeSec, float pressure, float imuTemp, float baroTemp, float GPSSats, float latitude, float longitude, float positionX) {
-    Serial1.print(ori.x, 3);                 Serial1.print(",");
-    Serial1.print(ori.y, 3);                 Serial1.print(",");
-    Serial1.print(ori.z, 3);                 Serial1.print(",");
-    Serial1.print(accel.x, 3);               Serial1.print(",");
-    Serial1.print(accel.y, 3);               Serial1.print(",");
-    Serial1.print(accel.z, 3);               Serial1.print(",");
-    Serial1.print(accelHG.x, 3);             Serial1.print(",");
-    Serial1.print(accelHG.y, 3);             Serial1.print(",");
-    Serial1.print(accelHG.z, 3);             Serial1.print(",");
-    Serial1.print(gyro.x, 3);                Serial1.print(",");
-    Serial1.print(gyro.y, 3);                Serial1.print(",");
-    Serial1.print(gyro.z, 3);                Serial1.print(",");
-    Serial1.print(altitude, 3);              Serial1.print(",");
-    Serial1.print(positionX, 3);             Serial1.print(",");
-    Serial1.print(rwValue, 3);               Serial1.print(",");
-    Serial1.print(batteryVoltage, 3);        Serial1.print(",");
-    Serial1.print(systemState);              Serial1.print(",");
-    Serial1.print(cameraState);              Serial1.print(",");
-    Serial1.print(reactionWheelState);       Serial1.print(",");
-    Serial1.print(onTimeSec, 3);             Serial1.print(",");
-    Serial1.print(flightTimeSec, 3);         Serial1.print(",");
-    Serial1.print(pressure, 3);              Serial1.print(",");
-    Serial1.print(imuTemp, 3);               Serial1.print(",");
-    Serial1.print(baroTemp, 3);              Serial1.print(",");
-    Serial1.print(GPSSats);                  Serial1.print(",");
-    Serial1.print(latitude, 4);              Serial1.print(",");
-    Serial1.println(longitude, 4);
+    File file = SD.open(filePath, O_CREAT | O_WRITE);
+    file.print(ori.x, 3);                 file.print(",");
+    file.print(ori.y, 3);                 file.print(",");
+    file.print(ori.z, 3);                 file.print(",");
+    file.print(accel.x, 3);               file.print(",");
+    file.print(accel.y, 3);               file.print(",");
+    file.print(accel.z, 3);               file.print(",");
+    file.print(accelHG.x, 3);             file.print(",");
+    file.print(accelHG.y, 3);             file.print(",");
+    file.print(accelHG.z, 3);             file.print(",");
+    file.print(gyro.x, 3);                file.print(",");
+    file.print(gyro.y, 3);                file.print(",");
+    file.print(gyro.z, 3);                file.print(",");
+    file.print(altitude, 3);              file.print(",");
+    file.print(positionX, 3);             file.print(",");
+    file.print(rwValue, 3);               file.print(",");
+    file.print(batteryVoltage, 3);        file.print(",");
+    file.print(systemState);              file.print(",");
+    file.print(cameraState);              file.print(",");
+    file.print(reactionWheelState);       file.print(",");
+    file.print(onTimeSec, 3);             file.print(",");
+    file.print(flightTimeSec, 3);         file.print(",");
+    file.print(pressure, 3);              file.print(",");
+    file.print(imuTemp, 3);               file.print(",");
+    file.print(baroTemp, 3);              file.print(",");
+    file.print(GPSSats);                  file.print(",");
+    file.print(latitude, 4);              file.print(",");
+    file.println(longitude, 4);
+
+    file.close();
     // ! IF NOT LOGGING FAST ENOUGH use Serial1.flush() to push all chars out before moving on! (blocking function)
 }
